5178309d7dfd0e16b1100771142f2622d06486b2
writingindy <writingindy@gmail.com>
2024-12-01 19:58:15 +0000

    Update web app

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,11 @@ nyiso_load = load_table_based_on_timerange('2019-01-01', None, 'nyiso_load')
 
 st.header('Exploratory Data Analysis', divider='gray')
 
+st.write(
+    "This is a plot of the Monthly Average Electricity Load for NYISO"
+)
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -74,7 +79,41 @@ plt.tight_layout()
 
 st.pyplot(fig)
 
-
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This is a plot of the Daily Average Electricity Load for NYISO over the week"
 )
+
+#Weekly
+daily_load = nyiso_load_copy.resample('D').mean()
+
+
+nyiso_load_copy['Year'] = nyiso_load_copy.index.year
+nyiso_load_copy['Weekday'] = nyiso_load_copy.index.weekday  # Monday=0, Sunday=6
+
+
+weekday_load_avg_per_year = nyiso_load_copy.groupby(['Year', 'Weekday']).mean().unstack(level=0)
+weekday_load_avg_overall = nyiso_load_copy.groupby('Weekday').mean()
+
+
+fig2 = plt.figure(figsize=(12, 6))
+
+for year in nyiso_load_copy['Year'].unique():
+    weekday_data = weekday_load_avg_per_year['Load'][year]
+    plt.plot(weekday_data.index, weekday_data, alpha=0.3, label=str(year))
+
+
+plt.plot(weekday_load_avg_overall.index, weekday_load_avg_overall['Load'], color='blue', linewidth=3, label='Average Load')
+
+
+plt.title('Historical New York Load Data - Daily Averages by Weekday')
+plt.xlabel('Weekday')
+plt.ylabel('Load (MW)')
+
+
+plt.xticks(range(0, 7), ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'])
+plt.xlim([0, 6])
+
+plt.legend(title='Year', loc='upper right', bbox_to_anchor=(1.05, 1))
+plt.grid(True)
+plt.tight_layout()
+st.pyplot(fig2)

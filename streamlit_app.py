cca020b05bc0325b0e54296297576e17143a744c
writingindy <writingindy@gmail.com>
2024-12-01 21:04:16 +0000

    Update app to have pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,20 @@ st.set_page_config(
     page_icon=':electric_plug:', # This is an emoji shortcode. Could be a URL too.
 )
 
+st.set_page_config(layout="wide")
+
+# If you want to use the no-sections version, this
+# defaults to looking in .streamlit/pages.toml, so you can
+# just call `get_nav_from_toml()`
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
+
+
+pg = st.navigation(nav)
+
+add_page_title(pg)
+
+pg.run()
+
 
 st.title("Electricity Data Dashboard")
 

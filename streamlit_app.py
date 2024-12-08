52edc3e961137ccfb420211d9bac5d2b59cbd0cb
writingindy <writingindy@gmail.com>
2024-12-08 04:07:39 +0000

    Update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,13 @@ import time
 import psycopg2
 from st_pages import add_page_title, get_nav_from_toml
 
+warnings.filterwarnings('ignore')
+
+st.set_page_config(
+    layout="wide",
+    page_title='Electricity Data Dashboard',
+    page_icon=':electric_plug:', # This is an emoji shortcode. Could be a URL too.
+)
 
 ### Global Variables and Helper Functions
 
@@ -29,83 +36,6 @@ caiso_fuel_sources = [
 
 isone_fuel_sources = [
     'coal', 'hydro', 'landfill_gas', 'natural_gas', 'nuclear', 'oil', 'refuse', 'solar', 'wind', 'wood', 'other']
-
-
-
-
-warnings.filterwarnings('ignore')
-
-st.set_page_config(
-    layout="wide",
-    page_title='Electricity Data Dashboard',
-    page_icon=':electric_plug:', # This is an emoji shortcode. Could be a URL too.
-)
-
-
-# If you want to use the no-sections version, this
-# defaults to looking in .streamlit/pages.toml, so you can
-# just call `get_nav_from_toml()`
-#nav = get_nav_from_toml(".streamlit/pages_sections.toml")
-
-
-#pg = st.navigation(nav)
-
-#add_page_title(pg)
-
-
-
-st.title(":electric_plug: Electricity Data Dashboard")
-
-
-'''
-This web app has two sections:
-- The first section will present a live dashboard of the current day's electricity load and fuel mix, as well as the forecasted load.
-- The second section will present some interactive exploratory data analysis on electricity load and fuel mix data, gathered from the gridstatus API.
-'''
-
-st.header('Live Dashboard', divider='gray')
-
-#@st.cache_data
-def get_day_data(table):
-    today = datetime.date.today()
-    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-    #today = pd.to_datetime('2024-11-26')
-    #tomorrow = pd.to_datetime('2024-11-27')
-
-    conn = st.connection("postgresql", type="sql")
-    
-    res = conn.query(f"SELECT * FROM {table} WHERE time >= \'{today}\' AND time < \'{tomorrow}\';", ttl="10m")
-    res = res.sort_values(by='time')
-
-    return res
-
-
-def plot_day_load(table):
-    data = get_day_data(table)
-    
-    fig = plt.figure(figsize=(12, 6))
-
-    data_copy = data.copy()
-
-    #data_copy['time'] = pd.to_datetime(data_copy['time'])
-    #data_copy.set_index('time', inplace=True)
-
-    #data_copy['Hour'] = data_copy.index.hour
-    
-    plt.plot(data_copy['time'], data_copy['load'], color='blue', linewidth=3, label='Average Load')
-
-    return fig
-    
-    
-
-#today_nyiso_load = get_day_data('nyiso_load')
-
-
-nyiso_tab, caiso_tab, isone_tab = st.tabs(["NYISO", "CAISO", "ISONE"])
-nyiso_tab.pyplot(plot_day_load('nyiso_load'))
-caiso_tab.pyplot(plot_day_load('caiso_load'))
-isone_tab.pyplot(plot_day_load('isone_load'))
-
 
 
 @st.cache_data
@@ -437,6 +367,63 @@ def plot_daily_table_based_on_timerange(timemin, timemax, table):
             plt.grid(True)
             plt.tight_layout()        
     return fig
+
+### Web App by Streamlit
+
+st.title(":electric_plug: Electricity Data Dashboard")
+
+'''
+This web app has two sections:
+- The first section will present a live dashboard of the current day's electricity load and fuel mix, as well as the forecasted load.
+- The second section will present some interactive exploratory data analysis on electricity load and fuel mix data, gathered from the gridstatus API.
+'''
+
+st.header('Live Dashboard', divider='gray')
+
+@st.cache_data
+def get_day_data(table):
+    today = datetime.date.today()
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    #today = pd.to_datetime('2024-11-26')
+    #tomorrow = pd.to_datetime('2024-11-27')
+
+    conn = st.connection("postgresql", type="sql")
+    
+    res = conn.query(f"SELECT * FROM {table} WHERE time >= \'{today}\' AND time < \'{tomorrow}\';", ttl="10m")
+    res = res.sort_values(by='time')
+
+    return res
+
+
+def plot_day_load(table):
+    data = get_day_data(table)
+    
+    fig = plt.figure(figsize=(12, 6))
+
+    data_copy = data.copy()
+
+    #data_copy['time'] = pd.to_datetime(data_copy['time'])
+    #data_copy.set_index('time', inplace=True)
+
+    #data_copy['Hour'] = data_copy.index.hour
+    
+    plt.plot(data_copy['time'], data_copy['load'], color='blue', linewidth=3, label='Average Load')
+
+    return fig
+    
+    
+
+#today_nyiso_load = get_day_data('nyiso_load')
+
+
+nyiso_tab, caiso_tab, isone_tab = st.tabs(["NYISO", "CAISO", "ISONE"])
+nyiso_tab.pyplot(plot_day_load('nyiso_load'))
+caiso_tab.pyplot(plot_day_load('caiso_load'))
+isone_tab.pyplot(plot_day_load('isone_load'))
+
+
+
+
 
 st.header('Exploratory Data Analysis', divider='gray')
 

cffae567ab174e0a6144b8810311a6236e8fcc2c
writingindy <writingindy@gmail.com>
2024-12-08 11:56:30 +0000

    Try again

--- pages/2_🔌_Realtime_Electricity_Data_Dashboard.py
+++ pages/2_🔌_Realtime_Electricity_Data_Dashboard.py
@@ -109,10 +109,10 @@ nyiso_tab, caiso_tab, isone_tab = st.tabs(["NYISO", "CAISO", "ISONE"])
 
 for five_min_interval in range(288):
     with nyiso_tab.container():
-        nyiso_load_placeholder = st.empty()
-        nyiso_fuel_mix_placeholder = st.empty()
-        nyiso_load_placeholder.pyplot(plot_day_data('nyiso_load'))
-        nyiso_fuel_mix_placeholder.pyplot(plot_day_data('nyiso_fuel_mix'))
+
+        nyiso_tab.pyplot(plot_day_data('nyiso_load'))
+        nyiso_tab.pyplot(plot_day_data('nyiso_fuel_mix'))
+
     caiso_tab.pyplot(plot_day_data('caiso_load'))
     caiso_tab.pyplot(plot_day_data('caiso_fuel_mix'))
     isone_tab.pyplot(plot_day_data('isone_load'))
